739210509e14483dd93c349466f9aca3acf6ee70
深蓝 <90918529+bilulanlv@users.noreply.github.com>
2025-03-31 11:41:23 +0800

    增加超时配置

--- src/config/plugin/tinywan/rpc/app.php
+++ src/config/plugin/tinywan/rpc/app.php
@@ -6,4 +6,6 @@ return [
         'namespace'=> 'service\\', // 自定义服务命名空间
         'listen_text_address' => 'text://0.0.0.0:9512', // 自定义Text协议地址
     ],
+    'connect_timeout' => 5, // 超时时间
+    'request_timeout' => 5, // 请求超时时间
 ];
