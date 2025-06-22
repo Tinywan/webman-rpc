12ec153d74c5272022d571d7252091b56a72e430
Tinywan <756684177@qq.com>
2025-06-22 17:10:50 +0800

    JSON-RPC 2.0

--- src/Protocol/RpcTextProtocol.php
+++ src/Protocol/RpcTextProtocol.php
@@ -12,8 +12,6 @@ declare(strict_types=1);
 namespace Tinywan\Rpc\Protocol;
 
 use Throwable;
-use Tinywan\ExceptionHandler\Logger;
-use Tinywan\Rpc\JsonParser;
 use Workerman\Connection\TcpConnection;
 
 class RpcTextProtocol
@@ -28,34 +26,79 @@ class RpcTextProtocol
         try {
             static $instances = [];
 
-            $data = json_decode($string, true);
-            $error = json_last_error();
-            if ($error != JSON_ERROR_NONE) {
-                return JsonParser::encode($connection, 400, sprintf('Data(%s) is not json format!', $string));
+            $request = json_decode($string, true);
+            if (!$request) {
+                // JSON RPC 2.0标准错误码-32700
+                $response = [
+                    'jsonrpc' => '2.0',
+                    'error' => [
+                        'code' => -32700,
+                        'message' => 'Parse error',
+                        'data' => 'Invalid JSON'
+                    ],
+                    'id' => null
+                ];
+                return $connection->send(json_encode($response) . "\n");
             }
 
+            // 验证JSON RPC 2.0格式
+            if ($request['jsonrpc'] !== '2.0') {
+                $response = [
+                    'jsonrpc' => '2.0',
+                    'error' => [
+                        'code' => -32600,
+                        'message' => 'Invalid Request',
+                        'data' => 'jsonrpc must be "2.0"'
+                    ],
+                    'id' => $request['id'] ?? null
+                ];
+                return $connection->send(json_encode($response) . "\n");
+            }
+
+            $classMethod = explode('.', $request['method']);
+            if (count($classMethod) !== 2) {
+                $response = [
+                    'jsonrpc' => '2.0',
+                    'error' => [
+                        'code' => -32601,
+                        'message' => 'Method not found',
+                        'data' => 'Invalid method format'
+                    ],
+                    'id' => $request['id'] ?? null
+                ];
+                return $connection->send(json_encode($response) . "\n");
+            }
+            [$className, $methodName] = $classMethod;
             $config = config('plugin.tinywan.rpc.app');
-            $class = $config['server']['namespace'] . $data['class'];
+            $class = $config['server']['namespace'] . $className;
             if (!class_exists($class)) {
-                return JsonParser::encode($connection, 404, sprintf('%s Class is not exist!', $data['class']));
+                throw new \Exception(sprintf('%s Class is not exist!', $className), 405);
             }
 
-            $method = $data['method'];
-            if (!method_exists($class, (string) $method)) {
-                return JsonParser::encode($connection, 404, sprintf('%s method is not exist!', $method));
+            if (!method_exists($class, (string)$methodName)) {
+                throw new \Exception(sprintf('%s method is not exist!', $methodName), 405);
             }
             $args = $data['args'] ?? [];
             if (!isset($instances[$class])) {
                 $instances[$class] = new $class();
             }
-            return $connection->send(call_user_func_array([$instances[$class], $method], $args));
+            return $connection->send(call_user_func_array([$instances[$class], $methodName], $args));
         } catch (Throwable $th) {
-            Logger::error('RPC Service Exception '.$th->getMessage(), [
-                'error' => $th->getMessage(),
-                'file' => $th->getFile(),
-                'line' => $th->getLine()
-            ]);
-            return JsonParser::encode($connection, 500, $th->getMessage());
+            $errorCode = -32000; // 自定义错误基础码
+            if ($th->getCode() === 404 || $th->getCode() === 405) {
+                $errorCode = -32601;
+            }
+
+            $response = [
+                'jsonrpc' => '2.0',
+                'error' => [
+                    'code' => $errorCode,
+                    'message' => $th->getMessage(),
+                    'data' => ['file' => $th->getFile(), 'line' => $th->getLine()]
+                ],
+                'id' => $request['id'] ?? null
+            ];
+            return $connection->send(json_encode($response) . "\n");
         }
     }
 }
